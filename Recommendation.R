aad07d915fd4912f15a2fa5a04b18b10199bf57b
ForeverMi <144679982+ForeverMi@users.noreply.github.com>
2023-10-22 22:06:51 +1300

    Update Recommendation.R

--- Recommendation.R
+++ Recommendation.R
@@ -1,8 +1,8 @@
-# 加载必要的库
+
 library(dplyr)
 library(data.table)
 
-# 读取相似度矩阵和数据集
+# Read the similarity matrix and dataset
 m1_similarity1 <- read.table("similarity_matrix.csv", header = FALSE)
 df_pivot1 <- read.csv("data_frame.csv", header = TRUE)
 
@@ -23,22 +23,22 @@ get_recommendations <- function(user_index) {
     user_b_index <- similar_users[i]
     user_b_recommendations <- df_pivot[user_b_index, ]
     
-    # 找到用户b推荐倾向最高的产品
+    # Find the products that have the highest propensity to be recommended by users
     max_recommendation <- names(user_b_recommendations[user_b_recommendations == max(user_b_recommendations)])
     
     if (length(max_recommendation) > 0) {
-      # 避免向同一用户重复推荐相同的产品
+      # Avoid recommending the same product to the same user over and over again
       max_recommendation <- setdiff(max_recommendation, recommendations)
       
       if (length(max_recommendation) > 0) {
         recommendations[i] <- max_recommendation[1]
       } else {
-        # 如果所有推荐产品都已经在recommendations中，随机选择一个产品
+        # If all the recommended products are already in the recommendations, randomly select a product
         available_products <- setdiff(names(df_pivot), recommendations)
         recommendations[i] <- sample(available_products, 1)
       }
     } else {
-      # 如果用户b没有推荐倾向最高的产品，随机选择一个产品
+      # If user b does not have the highest propensity to recommend a product, randomly select a product
       available_products <- setdiff(names(df_pivot), recommendations)
       recommendations[i] <- sample(available_products, 1)
     }
@@ -48,10 +48,10 @@ get_recommendations <- function(user_index) {
 }
 
 calculate_precision <- function(recommendations_df1, recommendations_df2) {
-  # 获取用户列表（假定用户列表是在 "User" 列中）
+  # Get the list of users (assuming the list of users is in the "User" column)
   users <- recommendations_df1$User
   
-  # 初始化一个向量来存储每个用户的相同产品数量
+  # Initialise a vector to store the same number of products per user
   precision <- numeric(length(users))
   
   for (i in 1:length(users)) {
@@ -59,7 +59,7 @@ calculate_precision <- function(recommendations_df1, recommendations_df2) {
     recommendations1 <- unlist(recommendations_df1[recommendations_df1$User == user, -1])
     recommendations2 <- unlist(recommendations_df2[recommendations_df2$User == user, -1])
     
-    # 计算相同产品数量
+    # Calculate the number of identical products
     common_recommendations <- intersect(recommendations1, recommendations2)
     precision[i] <- length(common_recommendations)
   }
@@ -69,21 +69,21 @@ calculate_precision <- function(recommendations_df1, recommendations_df2) {
 
 
 calculate_average_new_recommendations <- function(user_product_df, recommendations_df) {
-  # 初始化一个向量来存储每行用户的新产品推荐数
+  # Initialise a vector to store the number of new product recommendations per row of users
   new_recommendations_count <- integer(nrow(recommendations_df))
   
   for (i in 1:nrow(recommendations_df)) {
     user <- recommendations_df[i, 1]
-    recommended_products <- recommendations_df[i, -1]  # 去掉用户名列
+    recommended_products <- recommendations_df[i, -1]  # Remove user name list
     user_products <- user_product_df[user, ]
     
-    # 计算用户获得的新产品推荐数
+    # Calculate the number of new product referrals a user receives
     new_recommendations <- sum(recommended_products %in% names(user_products)[user_products == 0])
     
     new_recommendations_count[i] <- new_recommendations
   }
   
-  # 计算平均的新产品推荐数
+  # Calculate the average number of new product recommendations
   average_new_recommendations <- mean(new_recommendations_count)
   return(average_new_recommendations)
 }
@@ -98,15 +98,15 @@ recommendations2 <- t(sapply(users2, get_recommendations))
 recommendations_df2 <- data.frame(User = users2, recommendations2)
 
 
-# 使用函数计算平均产品推荐相同数
+# Calculate the average number of product recommendations using the function
 average_precision <- mean(calculate_precision(recommendations_df1, recommendations_df2))/8
 
-# 打印结果
-cat("平均产品推荐相同数（精确度）：", average_precision, "\n")
+# Print results
+cat("Average number of identical product recommendations (accuracy):", average_precision, "\n")
 
-# 传入用户-产品数据框和推荐列表数据框
+# Incoming user-product data frame and recommendation list data frame
 average_new_products1 <- calculate_average_new_recommendations(df_pivot1,recommendations_df1)
 
-# 输出平均的新产品数量
-cat("平均的新产品数量:", average_new_products1, "\n")
+# Output average number of new products
+cat("Average number of new products:", average_new_products1, "\n")
 
